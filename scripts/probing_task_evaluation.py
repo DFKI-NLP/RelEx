d62fda16ef051c7bcf32e992eeb280c9525a8ae1
Christoph Alt <christoph.alt@dfki.de>
2019-02-21 16:36:59 +0100

    Enable logging for probing task evaluation

--- scripts/probing_task_evaluation.py
+++ scripts/probing_task_evaluation.py
@@ -8,6 +8,11 @@ import reval
 from relex.predictors.utils import load_predictor
 
 logger = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s %(levelname)-8s %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
 
 
 ALL_PROBING_TASKS = [
