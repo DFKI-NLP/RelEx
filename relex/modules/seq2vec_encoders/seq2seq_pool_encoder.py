0b33fc201a0889264679a5bd6e6cf4089eb7549d
Marc <marchbnr@users.noreply.github.com>
2019-04-04 10:15:06 +0200

    Add pooling scopes to the seq2seq pooling wrapper

--- relex/modules/seq2vec_encoders/seq2seq_pool_encoder.py
+++ relex/modules/seq2vec_encoders/seq2seq_pool_encoder.py
@@ -1,17 +1,27 @@
+from typing import List
+
 import torch
 from overrides import overrides
 from allennlp.modules.seq2vec_encoders.seq2vec_encoder import Seq2VecEncoder
 from allennlp.modules.seq2seq_encoders.seq2seq_encoder import Seq2SeqEncoder
-from relex.modules.seq2vec_encoders.utils import pool
+from relex.modules.seq2vec_encoders.utils import PoolingScope, scoped_pool
 
 
 @Seq2VecEncoder.register("seq2seq_pool")
 class Seq2SeqPoolEncoder(Seq2VecEncoder):
-    def __init__(self, encoder: Seq2SeqEncoder, pooling: str = "max") -> None:
+    def __init__(self,
+                 encoder: Seq2SeqEncoder,
+                 pooling: str = "max",
+                 pooling_scope: List[str] = None) -> None:
         super().__init__()
 
         self._encoder = encoder
         self._pooling = pooling
+        if pooling_scope is None:
+            self._pooling_scope = [PoolingScope.SEQUENCE]
+        else:
+            self._pooling_scope = [PoolingScope(scope)
+                                   for scope in pooling_scope]
 
     @overrides
     def get_input_dim(self) -> int:
@@ -22,16 +32,23 @@ class Seq2SeqPoolEncoder(Seq2VecEncoder):
         return self._encoder.get_output_dim()
 
     def forward(
-        self, tokens: torch.Tensor, mask: torch.Tensor
+            self,
+            tokens: torch.Tensor,
+            mask: torch.Tensor,
+            head: torch.LongTensor = None,
+            tail: torch.LongTensor = None
     ):  # pylint: disable=arguments-differ
         if mask is not None:
             tokens = tokens * mask.unsqueeze(-1).float()
 
         tokens = self._encoder(tokens, mask)
-        return pool(
+
+        return scoped_pool(
             tokens,
-            mask.unsqueeze(-1),
-            dim=1,
+            mask,
             pooling=self._pooling,
+            pooling_scopes=self._pooling_scope,
             is_bidirectional=self._encoder.is_bidirectional(),
+            head=head,
+            tail=tail
         )
